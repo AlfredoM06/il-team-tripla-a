a52ef3ae9ac4a1a410a95350487694efcd68c170
AlesDod02 <alessandro.dod@gmail.com>
2024-01-12 16:21:13 +0100

    Prova 1

---
+++ src/main/java/com/example/provaTeamTriplaAAA/ProvaTeamTriplaAaaApplication.java
@@ -0,0 +1,13 @@
+package com.example.provaTeamTriplaAAA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProvaTeamTriplaAaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProvaTeamTriplaAaaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/provaTeamTriplaAAA/ProvaTeamTriplaAaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.provaTeamTriplaAAA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProvaTeamTriplaAaaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
